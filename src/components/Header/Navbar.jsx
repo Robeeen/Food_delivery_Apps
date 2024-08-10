09345265630e4ce6d605e81d31dfeaff69890e11
Robeeen <robeeen@gmail.com>
2024-08-10 21:39:50 +0600

    nav bar

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+  const [ menu, setMenu ] = useState("Home");
+
+
   return (
     <div className='navbar'>
         <img src={assets.logo} alt='Logo' className='logo' />
         <ul className='navbar-menu'>
-          <ul>home</ul>
-          <ul>menu</ul>
-          <ul>mobile-apps</ul>
-          <ul>contact us</ul>
+          <ul className={menu === "Home" ? "active" : ""}>Home</ul>
+          <ul className={menu === "Menu" ? "active" : ""}>Menu</ul>
+          <ul className={menu === "Mobile-apps" ? "active" : ""}>Mobile-apps</ul>
+          <ul className={menu === "Contact Us" ? "active" : ""} >Contact us</ul>
         </ul>
         <div className='navbar-right'>
           <img src={assets.search_icon} alt="" />
@@ -18,6 +21,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="" />
             <div className='dot'></div>
           </div>
+          <button>SignIn</button>
         </div>
     </div>
   )
