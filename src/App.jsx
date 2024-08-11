3fd115f710cf20ba2e3d04f3fcdbd84d0e40500c
Robeeen <robeeen@gmail.com>
2024-08-11 20:13:49 +0600

    all files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import Navbar from './components/Header/Navbar'
+import Navbar from './components/Navbar/Navbar'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/cart/Cart'
+import PlaceOrder from './pages/placeOrder/PlaceOrder'
+import { Home } from './pages/home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/order' element={ <Cart /> } />
+        <Route path='/cart' element={ <PlaceOrder /> } />
+      </Routes>
     </div>
   )
 }
