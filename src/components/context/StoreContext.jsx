f4ee17ccac1d9139b6b33458a2a5d8cd40cf0d02
Robeeen <robeeen@gmail.com>
2024-08-12 11:11:28 +0600

    pages

---
+++ src/components/context/StoreContext.jsx
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+
+    const ContextValue = {
+
+    }
+
+    return (
+        <StoreContext.Provider value={ContextValue} >
+            {props.children}
+        </StoreContext.Provider>
+    )
+}
+
+export default StoreContextProvider
