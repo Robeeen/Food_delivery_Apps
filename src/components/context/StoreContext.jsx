ee0469c7f391fb02562dd6f45bcf10c705dc0de0
Robeeen <robeeen@gmail.com>
2024-08-12 20:33:53 +0600

    Context

--- src/components/context/StoreContext.jsx
+++ src/components/context/StoreContext.jsx
@@ -1,9 +1,23 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+
+    const [ cartItems, setCartItems ] = useState({});
+
+    const addToCart = (itemID) => {
+            if(!cartItems[itemID]){
+                setCartItems((prev) => ({...prev, [itemID] : 1}))
+            }else{
+                setCartItems((prev) => ({...prev, [itemID] : prev[itemID] + 1}))
+            }
+    }
+
+    const removeCart = () => {
+
+    }
 
     const ContextValue = {
         food_list
