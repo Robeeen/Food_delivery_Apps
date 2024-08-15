1affa10224280c1924366200f43286662587b21c
Robeeen <robeeen@gmail.com>
2024-08-15 20:44:56 +0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,19 +1,27 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
     <div className="footer" id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-
+            <img src={assets.logo} alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sapiente id aspernatur reiciendis aliquam repellendus aut beatae optio obcaecati commodi quibusdam quis aperiam exercitationem, harum repellat a excepturi, ipsam dicta?
+            </p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
             </div>
             <div className="footer-content-center">
 
             </div>
 
             <div className="footer-content-right">
-                
+
             </div>
         </div>
 
