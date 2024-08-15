aafafcf1cccb02ecd4b8071dc1fdd4484ebc62c3
Robeeen <robeeen@gmail.com>
2024-08-15 20:47:16 +0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,30 +3,41 @@ import './Footer.css'
 import { assets } from '../../assets/assets'
 
 const Footer = () => {
-  return (
-    <div className="footer" id='footer'>
-        <div className="footer-content">
-            <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sapiente id aspernatur reiciendis aliquam repellendus aut beatae optio obcaecati commodi quibusdam quis aperiam exercitationem, harum repellat a excepturi, ipsam dicta?
-            </p>
-            <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
-            </div>
-            </div>
-            <div className="footer-content-center">
+    return (
+        <div className="footer" id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio sapiente id aspernatur reiciendis aliquam repellendus aut beatae optio obcaecati commodi quibusdam quis aperiam exercitationem, harum repellat a excepturi, ipsam dicta?
+                    </p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Delivery</li>
+                        <li>Privacy Policy</li>
+                    </ul>
 
+                </div>
+
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+0155235689-8888</li>
+                        <li>Email: Gmail.comta.@gmail.com</li>
+                    </ul>
+                </div>
             </div>
 
-            <div className="footer-content-right">
-
-            </div>
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Footer
