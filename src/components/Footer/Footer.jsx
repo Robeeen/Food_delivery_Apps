42e4f34d63edf18ee522cc2aa2181af8fa5a97ca
Robeeen <robeeen@gmail.com>
2024-08-15 20:42:04 +0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,21 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer" id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+
+            </div>
+            <div className="footer-content-center">
+
+            </div>
+
+            <div className="footer-content-right">
+                
+            </div>
+        </div>
+
+    </div>
   )
 }
 
