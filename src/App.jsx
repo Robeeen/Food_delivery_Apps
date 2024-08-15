d9ee497d05f06701ca88b7bc55c662f05d573b56
Robeeen <robeeen@gmail.com>
2024-08-15 20:39:19 +0600

    Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,21 @@ import { Route, Routes } from 'react-router-dom'
 import Cart from './pages/cart/Cart'
 import PlaceOrder from './pages/placeOrder/PlaceOrder'
 import { Home } from './pages/home/Home'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={ <Home />} />
-        <Route path='/order' element={ <Cart /> } />
-        <Route path='/cart' element={ <PlaceOrder /> } />
-      </Routes>
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/order' element={<Cart />} />
+          <Route path='/cart' element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   )
 }
 
