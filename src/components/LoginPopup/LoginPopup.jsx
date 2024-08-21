f2f0671f9c56187420e1a6c9d72ea99e3f36495b
Robeeen <robeeen@gmail.com>
2024-08-21 22:46:35 +0600

    Working on Popup Login

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -4,14 +4,21 @@ import { assets } from '../../assets/assets'
 
 const LoginPopup = ({setShowLogin}) => {
 
-    const [ curState, setCurState ] = useState("Sign up")
+    const [ curState, setCurState ] = useState("Login");
+
   return (
     <div className='login-popup'>
         <form action="" className="login-popup-container">
             <div className="login-popup-title">
-                <h2>{curState}</h2>
+                <h2>{ curState }</h2>
                 <img src={assets.cross_icon} onClick={() => setShowLogin(false)} />
             </div>
+            <div className='login-popup-input'>
+                { curState==="Login" ? <></> : <input type='text' placeholder='Your name' required /> }                
+                <input type='email' placeholder='Your Email' required />
+                <input type='password' placeholder='Password' required />
+            </div>
+            <button>{ curState==="Sign Up" ? "Create Account" : "Login" }</button>
         </form>
     </div>
   )
