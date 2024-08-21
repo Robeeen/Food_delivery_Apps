93db9a97b2bd9e8aff1a476ee8519350886c67c0
Robeeen <robeeen@gmail.com>
2024-08-21 11:31:39 +0600

    Working on Popup Login

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+    const [ curState, setCurState ] = useState("Sign up")
   return (
-    <div>LoginPopup</div>
+    <div className='login-popup'>
+        <form action="" className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{curState}</h2>
+                <img src={assets.cross_icon} onClick={() => setShowLogin(false)} />
+            </div>
+        </form>
+    </div>
   )
 }
 
