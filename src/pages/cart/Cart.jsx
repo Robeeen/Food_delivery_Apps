87137201805f69a97fd3b7b1c5aafe6501ebc207
Robeeen <robeeen@gmail.com>
2024-08-22 19:10:22 +0600

    Working on add to cart page

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../../components/context/StoreContext'
 
 
 const Cart = () => {
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext)
+  const { cartItems, food_list, removeCart } = useContext(StoreContext)
 
   return (
     <div className='cart'>
@@ -23,14 +23,17 @@ const Cart = () => {
           food_list.map((item, index) => {
             if(cartItems[item._id] > 0){
               return (
+                <>
                 <div className='cart-item-title cart-item-item'>
                   <img src={item.image} alt="" />
                   <p>{item.name}</p>
                   <p>{item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>{item.price * cartItems[item._id]}</p>
-                  <p>X</p>
+                  <p className='cross' onClick={() => removeCart(item._id)}>X</p>
                   </div>
+                  <hr />
+                </>
               )
             }
           })
