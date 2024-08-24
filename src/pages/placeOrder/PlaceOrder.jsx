fa9fa501230ea63f4360efc8ec963548e5deb3b7
Robeeen <robeeen@gmail.com>
2024-08-24 21:58:19 +0600

    Order Placeing

--- src/pages/placeOrder/PlaceOrder.jsx
+++ src/pages/placeOrder/PlaceOrder.jsx
@@ -12,7 +12,15 @@ const PlaceOrder = () => {
           <input type="text" PlaceOrder="Last Name" />
         </div>
         <input type="email" PlaceOrder="Email Address" />
-        <input type="text" PlaceOrder="Last Name" />
+        <input type="text" PlaceOrder="Street" />
+        <div className="multi-fields">
+          <input type="text" PlaceOrder="City" />
+          <input type="text" PlaceOrder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" PlaceOrder="Zip Code" />
+          <input type="text" PlaceOrder="Country" />
+        </div>
       </div>
       <div className="place-order-left">
 
